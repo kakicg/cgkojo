745fc5fd5c3909b41227ae75b4733ab72ef06564
Mikio Kakiyama <kakicg@me.com>
2023-11-03 04:09:39 +0900

    auth

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# cgkojo
-
-WEBサイト
+＃CGKoJo
---
+++ app/layout.tsx
@@ -0,0 +1,59 @@
+import "./globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { getAuthSession } from "@/lib/nextauth"
+import AuthProvider from "@/components/providers/AuthProvider"
+import TrpcProvider from "@/components/providers/TrpcProvider"
+import ToastProvider from "@/components/providers/ToastProvider"
+import Navigation from "@/components/auth/Navigation"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: 'CGKoJo',
+  description: 'クラシックギターと日々雑感',
+}
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+const RootLayout = async ({ children }: RootLayoutProps) => {
+  // 認証情報取得
+  const user = await getAuthSession()
+
+  return (
+    <html lang="ja">
+      <body className={inter.className}>
+        <div className="flex min-h-screen flex-col">
+          <AuthProvider>
+            <TrpcProvider>
+              <Navigation user={user} />
+              <ToastProvider />
+              <main className="container mx-auto max-w-screen-md flex-1 px-2">
+                  {children}
+                </main>
+
+                {/* フッター */}
+                <footer className="py-5">
+                  <div className="text-center text-sm">
+                    Copyright © All rights reserved |{" "}
+                    <a
+                      href="http://cgkojo.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underline"
+                    >
+                      CGKoJo
+                    </a>
+                  </div>
+                </footer>
+            </TrpcProvider>
+          </AuthProvider>
+        </div>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
